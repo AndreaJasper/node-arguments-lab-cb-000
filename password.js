28c4c642ded51f9242a2daa1821e28f0a4b8a21a
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2021-05-14 15:14:14 +0000

    Automatically backed up by Learn

--- password.js
+++ password.js
@@ -1,15 +1,14 @@
-function genPassword(length) {
-  var length = length || 8;
-    charset = "abcdefghijklmnopqrstuvwxyz0123456789",
-    retVal = "";
-  for (var i = 0; n = charset.length; i <length; i++) {
-    retVal += charset.cartAt(Math.floor(Math.random() * n));
-  }
-  return retVal;
+function generatePassword(length) {
+    var length = length || 8,
+        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+        retVal = "";
+    for (var i = 0, n = charset.length; i < length; i++) {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return retVal;
 }
 
-for (var i = 1; i<+ process.argv[3]; i++) {
-  console.log(genPassword(process.argv[2]));
+for (var i=1; i<= process.argv[3]; i++) {
+  console.log( generatePassword(process.argv[2]));
 }
-
-process.exit(0);
+ process.exit(0);
